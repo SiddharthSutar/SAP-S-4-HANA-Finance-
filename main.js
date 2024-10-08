012736f3e718e3d9ae3ef31e7c02c095cbbbe063
SiddharthSutar <93462368+SiddharthSutar@users.noreply.github.com>
2024-10-08 14:40:01 +1300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function enrollNow() {
+    alert('Thank you for showing interest! Our team will get back to you soon.');
+}
+
+function viewMore() {
+    alert('Redirecting to blog post...');
+}
+function redirectToCourses() {
+    window.location.href = "courses.html";
+}
